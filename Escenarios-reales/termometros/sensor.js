d37825528cac928c7e095f46e7f8ae9dac8fbeae
damcipolat <damian.cipolat@gmail.com>
2017-10-26 00:06:04 -0300

    subo avance

--- Escenarios-reales/termometros/sensor.js
+++ Escenarios-reales/termometros/sensor.js
@@ -1,16 +1,64 @@
-var amqp = require('amqplib/callback_api');
+//Incluyo el modulo de rabbitmq.
+const amqp   = require('amqplib/callback_api');
+let   conn   = null;
 
-amqp.connect('amqp://localhost', function(err, conn) {
-  conn.createChannel(function(err, ch) {
-    var ex = 'direct_logs';
-    var args = process.argv.slice(2);
-    var msg = args.slice(1).join(' ') || 'Hello World!';
-    var severity = (args.length > 0) ? args[0] : 'info';
+//Hace random entre dos rangos.
+const randomize  = (min,max)=>{
+  return Math.random()*(max-min)+min;
+}
 
-    ch.assertExchange(ex, 'direct', {durable: false});
-    ch.publish(ex, severity, new Buffer(msg));
-    console.log(" [x] Sent %s: '%s'", severity, msg);
-  });
+//Cuando se crea el canal.
+const createChannel = (err,ch)=>{
 
-  setTimeout(function() { conn.close(); process.exit(0) }, 500);
+  if (err)
+    console.log("Hubo un erro al crear un nuevo canal.");
+  else{
+
+      const ex  = 'ex';
+      //Creo el exchange.
+      ch.assertExchange(ex, 'direct', {durable: false});
+
+      //Inicio un ciclo de revisión y envio de datos cada x tiempo.
+      setInterval(()=>{
+       
+        //Actualizo la info. del sensor.
+        sensor.temp = Math.round(randomize(10,30));
+
+        //Armo el mensaje.
+        let msg = "SensID: "+sensor.id.toString()+" / "+sensor.temp.toString()+" °C";
+
+        //Publico al exchange.
+        ch.publish(ex, 'temp_data', new Buffer(msg));
+
+        console.log("> Send: "+msg);
+
+      }, 5000);
+
+  }
+
+}
+
+//Información del sensor.
+const sensor = {id : Math.round(randomize(1000,2000)), temp: Math.round(randomize(10,30)) };
+
+//Cuando me conecto a rabbitmq.
+amqp.connect('amqp://localhost', (err, connection)=>{
+
+  if (err)
+    console.log("> No se pudo conectar a la cola");
+  else{
+
+    conn = connection;  
+    conn.createChannel(createChannel);
+
+  }
+  
 });
+
+
+//Inicio.
+console.log("----------------------------");
+console.log(" SENSOR N°: "+sensor.id);
+console.log("----------------------------");
+console.log("- Haciendo lecturas cada 5 segundos.")
+console.log("");
--- Escenarios-reales/termometros/server.js
+++ Escenarios-reales/termometros/server.js
@@ -1,24 +1,18 @@
-var amqp = require('amqplib/callback_api');
+const amqp = require('amqplib/callback_api');
 
-var args = process.argv.slice(2);
-
-if (args.length == 0) {
-  console.log("Usage: receive_logs_direct.js [info] [warning] [error]");
-  process.exit(1);
-}
 
 amqp.connect('amqp://localhost', function(err, conn) {
   conn.createChannel(function(err, ch) {
-    var ex = 'direct_logs';
+    var ex = 'ex';
 
     ch.assertExchange(ex, 'direct', {durable: false});
 
     ch.assertQueue('', {exclusive: true}, function(err, q) {
-      console.log(' [*] Waiting for logs. To exit press CTRL+C');
+      console.log(' [*] Esperando datos de sensores');
 
-      args.forEach(function(severity) {
-        ch.bindQueue(q.queue, ex, severity);
-      });
+      
+      ch.bindQueue(q.queue, ex, "temp_data");
+      
 
       ch.consume(q.queue, function(msg) {
         console.log(" [x] %s: '%s'", msg.fields.routingKey, msg.content.toString());
